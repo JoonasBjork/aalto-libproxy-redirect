130e75b3cf05f02c0514ecb61997fe0fa0aab546
JoonasBjork <73201140+JoonasBjork@users.noreply.github.com>
2025-01-21 10:57:12 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,10 +1,6 @@
 // ==UserScript==
 // @name         Use aalto libroxy
-// @namespace    https://ieeexplore.ieee.org/*
-// @namespace    https://dl.acm.org/*
-// @namespace    https://www.sciencedirect.com/*
 // @version      2024-01-26
-// @description  try to take over the world!
 // @author       You
 // @match        https://ieeexplore.ieee.org/*
 // @match        https://dl.acm.org/*
@@ -12,7 +8,6 @@
 // @match        https://link.springer.com/*
 // @match        https://www.talouselama.fi/*
 // @match        https://www.tekniikkatalous.fi/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // @grant        none
 // ==/UserScript==
 
